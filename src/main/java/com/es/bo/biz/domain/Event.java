1f9fda5e665c247567a73807a9b2b6ca15097e65
Moben <munir7824@gmail.com>
2016-04-06 20:35:35 -0700

    fixed date issue

--- src/main/java/com/es/bo/biz/domain/Event.java
+++ src/main/java/com/es/bo/biz/domain/Event.java
@@ -1,6 +1,5 @@
 package com.es.bo.biz.domain;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -14,6 +13,44 @@ public class Event extends AbstractEntity {
     private Date eventStartDate;
     private Date eventEndDate;
     private String street;
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	public String getLongDescription() {
+		return longDescription;
+	}
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+	public Date getEventStartDate() {
+		return eventStartDate;
+	}
+	public void setEventStartDate(Date eventStartDate) {
+		this.eventStartDate = eventStartDate;
+	}
+	public Date getEventEndDate() {
+		return eventEndDate;
+	}
+	public void setEventEndDate(Date eventEndDate) {
+		this.eventEndDate = eventEndDate;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+    
+    
 
 
 }
